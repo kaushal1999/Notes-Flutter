4febb27d3dca57379a49dc763cde3c87dfea2f13
kaushal1999 <56195939+kaushal1999@users.noreply.github.com>
2023-01-02 11:50:12 +0530

    Delete lib/providers directory

--- lib/providers/fetch_user_details_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:utility_manager_flutter/services/user_details_service.dart';
-import 'package:supabase/supabase.dart';
-
-final getSingleUserDetailsProvider =
-    FutureProvider.autoDispose<PostgrestResponse>(
-  (_) => UserDetailsService().getSingleUserDetails(),
-);
--- lib/providers/search_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:utility_manager_flutter/services/search_service.dart';
-
-final urlSearchNotifier = Provider(
-  (_) => URLSearchService(),
-);
